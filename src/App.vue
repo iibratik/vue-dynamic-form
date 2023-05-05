d33eabc69b8bda3bc111b718d26d76aa57c8c222
iibratik <iibragimov2417@gamil.com>
2023-05-05 13:13:59 +0500

    some changes

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,16 @@
   <form class="form-content" action="#">
     <div class="form-inputs">
       <div class="inputs" v-for="(input, index) in params.inputs" :key="index.id">
-        <input  :type="input.type" v-if="input.type === 'text' && input.id === 1" :placeholder="input.placeholder" :id="'input-' + input.id">
-        <input class="form-control" :type="input.type"  v-if="input.type === 'email' && input.id === 5" :placeholder="input.placeholder" :id="'input-' + input.id">
-        <div :type="input.type" v-if="input.type === 'radio' && input.id === 3">
+        <input  :type="input.type" v-if="input.type === 'text'" :placeholder="input.placeholder" :id="'input-' + input.id">
+        <input class="form-control" :type="input.type"  v-if="input.type === 'email'" :placeholder="input.placeholder" :id="'input-' + input.id">
+        <div :type="input.type" v-if="input.type === 'radio'">
           <label :for="'input-' + input.id">{{ input.placeholder }}</label>
           <div v-for="(option, optionIndex) in input.values" :key="optionIndex">
             <label :for="'option-' + optionIndex">{{ option.text }}</label>
             <input  :type="input.type" :id="'option-' + optionIndex" :name="option.name" :value="option.value">
           </div>
         </div>
-        <div class="select-content" :style="{gridTemplateColumns: `repeat(${input['selector-per-block']}, 100px)` }" v-if="input.type === 'select' && input.id === 4">
+        <div class="select-content" :style="{gridTemplateColumns: `repeat(${input['selector-per-block']}, 100px)` }" v-if="input.type === 'select'">
         <select >
           <option v-for="(option, optionIndex) in input.values" :value="option.value" :key="optionIndex">{{ option.text }}</option>
         </select>
@@ -21,11 +21,11 @@
         </select>
 
         </div>
-        <div v-if="input.type === 'checkbox' && input.id === 6">
+        <div v-if="input.type === 'checkbox'">
           <input :type="input.type" :id="'input-' + input.id" :value="input.value">
           <label :for="'input-' + input.id">{{ input.placeholder }}</label>
         </div>
-        <div v-if="input.type === 'datepicker' && input.id === 2">
+        <div v-if="input.type === 'datepicker'">
           <label :for="'input-' + input.id">{{ input.placeholder }}</label>
           <VueDatePicker class="datepicker" v-model="date"></VueDatePicker>
         </div>
@@ -38,7 +38,7 @@
 <script>
 import VueDatePicker from '@vuepic/vue-datepicker';
 import '@vuepic/vue-datepicker/dist/main.css';
-import myParams from '@/assets/params.json';
+import myParams from '../params.json';
 export default {
   data() {
     return {
--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store'
 import "@/assets/style/main.scss"
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
 import $ from 'jquery';
 import Popper from 'popper.js';
 
